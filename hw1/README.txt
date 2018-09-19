588fa491c622db1de9c3a83dcf038311bbcc3d3d
Cameron Betz <cameronleebetz@gmail.com>
2018-09-19 10:28:16 -0600

    Added hw1 as binary file copy assignment

---
+++ hw1/README.txt
@@ -0,0 +1,6 @@
+README
+COPY homework
+
+This C++ program uses ios::binary rdbuf(); functionality to copy the 
+binary values of any file to a new source.
+from.ogv-->to.ogv (arbitrary non-text file, values can be changed)
--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -1,1 +1,13 @@
 //cameron betz
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    ifstream source("from.ogv", ios::binary);
+    ofstream destination("to.ogv", ios::binary);
+
+    destination << source.rdbuf();
+    return 0;
+}
