bddf496d653e0fcf7ca659aaa7c05a111afd5e2c
Cameron Betz <cameronleebetz@gmail.com>
2018-10-17 10:05:21 -0600

    added incompleate hw2 solution and updated readme

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 --HW1: Create a program that copies a binary file from one location to another
 
 path: /computernetworks2018/hw1/
+
+--HW2: Create a program that copies a binary file from one location to another over a network
+
+path: /computernetworks2018/hw2/
---
+++ hw2/socket.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket()
+address = '127.0.0.1'
+port = 80  # port number is a number, not string
+try:
+    s.connect((address, port)) 
+    s.send()
+
+
+except Exception as e: 
+    print("something's wrong with %s:%d. Exception is %s" % (address, port, e))
+finally:
+    s.close()
